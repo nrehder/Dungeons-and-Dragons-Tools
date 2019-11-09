0d362a513b8b10a7a4ead41e22e89e93ec522337
nrehder <nrehder@gmail.com>
2019-11-09 12:06:37 -0500

    started life events

---
+++ src/lifeEvents/lifeEvent.js
@@ -0,0 +1,34 @@
+import React from "react";
+import rollDice from "../randomizers/diceRoller";
+import jsonData from "./lifeEvents.json";
+
+const LifeEvent = props => {
+	const roll = rollDice(1, 100);
+	let event;
+	if (roll < 51) {
+		event = Math.ceil(roll / 10);
+	} else if (roll < 71) {
+		event = 6;
+	} else if (roll < 76) {
+		event = 7;
+	} else if (roll < 81) {
+		event = 8;
+	} else if (roll < 86) {
+		event = 9;
+	} else if (roll < 91) {
+		event = 10;
+	} else if (roll < 96) {
+		event = 11;
+	} else if (roll < 100) {
+		event = 12;
+	} else {
+		event = 13;
+	}
+	return (
+		<p>
+			{roll}: {jsonData["Life_Events"][event]}
+		</p>
+	);
+};
+
+export default LifeEvent;
--- src/lifeEvents/lifeEvents.js
+++ src/lifeEvents/lifeEvents.js
@@ -1,9 +1,38 @@
 import React from "react";
+import rollDice from "../randomizers/diceRoller";
+import LifeEvent from "./lifeEvent";
+
+const eventsDice = age => {
+	if (age > 60) {
+		return 12;
+	} else if (age > 50) {
+		return 10;
+	} else if (age > 40) {
+		return 8;
+	} else if (age > 30) {
+		return 6;
+	} else if (age > 20) {
+		return 4;
+	} else {
+		return 1;
+	}
+};
 
 const LifeEvents = props => {
+	const diceMax = eventsDice(props.age);
+	const numEvents = rollDice(1, diceMax);
+	console.log(diceMax);
+	console.log(numEvents);
+
+	const eventsResults = [];
+
+	for (let i = 0; i < numEvents; i++) {
+		eventsResults.push(<LifeEvent key={i} />);
+	}
 	return (
 		<div>
 			<h1>Life Events</h1>
+			{eventsResults}
 		</div>
 	);
 };
